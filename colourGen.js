bbd016a8ce4a3404f74e8588cbde7e7c07360074
Jessica <85376230+criminy-git@users.noreply.github.com>
2022-01-17 21:19:26 +1100

    created colourGen.js

---
+++ colourGen.js
@@ -0,0 +1,19 @@
+const colourGen = () => {
+    var letters = "0123456789abcdef";
+    var hashtag = ["#","#","#","#","#","#"];
+    for (let i=0; i<6; i++) {
+        hashtag[i]+=letters[Math.floor(Math.random() *16)];
+        hashtag[i]+=letters[Math.floor(Math.random() *16)];
+        hashtag[i]+=letters[Math.floor(Math.random() *16)];
+        hashtag[i]+=letters[Math.floor(Math.random() *16)];
+        hashtag[i]+=letters[Math.floor(Math.random() *16)];
+        hashtag[i]+=letters[Math.floor(Math.random() *16)];
+    }
+    for (let i=0; i<code.length; i++){
+        code[i].innerHTML = hashtag[i];
+        colour[i].style.backgroundColor = hashtag[i];
+    }
+}
+colourGen()
+
+export default colourGen
