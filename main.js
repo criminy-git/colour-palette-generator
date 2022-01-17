d13f48a5156166f8ee85e068cd046341736f99d0
Jessica <85376230+criminy-git@users.noreply.github.com>
2022-01-17 21:18:33 +1100

    Update main.js

--- main.js
+++ main.js
@@ -4,23 +4,7 @@ var button = document.querySelector('button');
 
 button.addEventListener("click",colourGen);
 
-function colourGen() {
-    var letters = "0123456789abcdef";
-    var hashtag = ["#","#","#","#","#","#"];
-    for (let i=0; i<6; i++) {
-        hashtag[i]+=letters[Math.floor(Math.random() *16)];
-        hashtag[i]+=letters[Math.floor(Math.random() *16)];
-        hashtag[i]+=letters[Math.floor(Math.random() *16)];
-        hashtag[i]+=letters[Math.floor(Math.random() *16)];
-        hashtag[i]+=letters[Math.floor(Math.random() *16)];
-        hashtag[i]+=letters[Math.floor(Math.random() *16)];
-    }
-    for (let i=0; i<code.length; i++){
-        code[i].innerHTML = hashtag[i];
-        colour[i].style.backgroundColor = hashtag[i];
-    }
-}
-colourGen()
+
 
 window.onkeyup = (e)=> {
     if (e.which||e.keycode == 33) {
