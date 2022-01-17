893d876f909bb024b96de942c83dc035024264ac
Jessica <85376230+criminy-git@users.noreply.github.com>
2022-01-17 21:31:30 +1100

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
-var code = document.querySelectorAll('.code');
-var colour = document.querySelectorAll('.colour');
-var button = document.querySelector('button');
+
 
 import colourGen from './colourGen.js'
 colourGen()
