6d17f012d5bc3d9656dd9b44f31ef6b7e4787dd9
Jessica <85376230+criminy-git@users.noreply.github.com>
2022-01-17 21:20:40 +1100

    updated main.js to be modularised

--- main.js
+++ main.js
@@ -2,9 +2,10 @@ var code = document.querySelectorAll('.code');
 var colour = document.querySelectorAll('.colour');
 var button = document.querySelector('button');
 
+import colourGen from './colourGen.js'
+colourGen()
+
 button.addEventListener("click",colourGen);
-
-
 
 window.onkeyup = (e)=> {
     if (e.which||e.keycode == 33) {
